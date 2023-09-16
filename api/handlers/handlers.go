8b292cd7790bd8346558b2a6e90c86378995b733
solomonbaez <solomon.james.baez@gmail.com>
2023-09-15 22:10:25 -0700

    Fixed validation error preventing JSON parse

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,14 +13,16 @@ var subscribers = make(map[string]models.Subscriber)
 func Subscribe(c *gin.Context) {
 	var subscriber models.Subscriber
 
-	_, e := subscribers[subscriber.Email]
-	if e {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Email already associated with a subscriber"})
-		return
-	}
-
 	if e := c.ShouldBindJSON(&subscriber); e != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Could not subscribe: " + e.Error()})
+	}
+
+	_, e := subscribers[subscriber.Email]
+	fmt.Printf("%v", e)
+	if e {
+		fmt.Printf("Cannot subscribe")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Email already associated with a subscriber"})
+		return
 	}
 
 	subscribers[subscriber.Email] = subscriber
