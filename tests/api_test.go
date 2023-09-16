e50ebddf14aa135c168da34e7fc4a1b989d68089
solomonbaez <solomon.james.baez@gmail.com>
2023-09-16 13:50:10 -0700

    Encapsulated server logic for /tests and renamed api testing suite

---
+++ tests/api_test.go
@@ -0,0 +1,52 @@
+package api_test
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/solomonbaez/SB-Go-Newsletter-API/api/handlers"
+)
+
+type App struct {
+	recorder *httptest.ResponseRecorder
+	router   *gin.Engine
+}
+
+func spawn_app(request *http.Request) App {
+	recorder := httptest.NewRecorder()
+
+	router := gin.Default()
+	router.GET("/health", handlers.HealthCheck)
+	router.ServeHTTP(recorder, request)
+
+	var new_app App
+	new_app = App{
+		recorder,
+		router,
+	}
+
+	return new_app
+}
+
+func TestHealthCheckReturnsOK(t *testing.T) {
+	// server initialization
+	request, e := http.NewRequest("GET", "/health", nil)
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	var app App
+	app = spawn_app(request)
+
+	// tests
+	if status := app.recorder.Code; status != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, status)
+	}
+
+	expectedBody := `"OK"`
+	if app.recorder.Body.String() != expectedBody {
+		t.Errorf("Expected body %s, but got %s", expectedBody, app.recorder.Body.String())
+	}
+}
