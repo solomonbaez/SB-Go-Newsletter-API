9c817775271d3c26cb416263b4e254e2b53f0515
solomonbaez <solomon.james.baez@gmail.com>
2023-09-16 21:02:48 -0700

    Added logging to postgres initialization script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,12 +1,21 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "ERROR: psql is not installed"
+    exit 1
+fi
+
 # configurations
 DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASS="${POSTGRES_PASS:=password}"
 DB_NAME="${POSTGRES_NAME:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
+
+# # DEV - remove test container
+# docker stop newsletter
+# docker rm newsletter
 
 docker run \
     --name newsletter \
@@ -15,3 +24,14 @@ docker run \
     -e POSTGRES_DB=${DB_NAME} \
     -p "${DB_PORT}":5432 \
     -d postgres  
+
+export PG_PASS="${DB_PASS}"
+until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+done
+
+>&2 echo "Success! Postgres is running on port ${DB_PORT}!"
+
+DB_URL=postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
