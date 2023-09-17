fd800a1bae19060476996f662dbf2b8e0f9de6b1
solomonbaez <solomon.james.baez@gmail.com>
2023-09-16 17:25:58 -0700

    Added test for Subscribe

--- tests/api_test.go
+++ tests/api_test.go
@@ -1,10 +1,12 @@
 package api_test
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/handlers"
 )
@@ -14,6 +16,7 @@ var router *gin.Engine
 func init() {
 	router = gin.Default()
 	router.GET("/health", handlers.HealthCheck)
+	router.POST("/subscribe", handlers.Subscribe)
 	router.GET("/subscribers", handlers.GetSubscribers)
 }
 
@@ -54,7 +57,7 @@ func TestHealthCheckReturnsOK(t *testing.T) {
 	}
 }
 
-func TestGetSubscribers(t *testing.T) {
+func TestGetSubscribersNoSubscribers(t *testing.T) {
 	// server initialization
 	request, e := http.NewRequest("GET", "/subscribers", nil)
 	if e != nil {
@@ -74,4 +77,43 @@ func TestGetSubscribers(t *testing.T) {
 	if response_body != expected_body {
 		t.Errorf("Expected body %v, but got %v", expected_body, response_body)
 	}
+}
+
+func TestGetSubscribersWithSubscribers(t *testing.T) {
+	// server initialization
+	data := `{"email": "test@test.com", "name": "test"}`
+	post_request, e := http.NewRequest("POST", "/subscribe", strings.NewReader(data))
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	app := spawn_app(post_request)
+
+	//  test POST
+	if status := app.recorder.Code; status != http.StatusCreated {
+		t.Errorf("Expected status code %v, but got %v", http.StatusCreated, status)
+	}
+
+	// test GET
+	t.Run("GetSubscribers", func(t *testing.T) {
+		get_request, e := http.NewRequest("GET", "/subscribers", nil)
+		if e != nil {
+			t.Fatal(e)
+		}
+
+		app.recorder = httptest.NewRecorder()
+		app.router.ServeHTTP(app.recorder, get_request)
+
+		// tests
+		if status := app.recorder.Code; status != http.StatusOK {
+			t.Errorf("Expected status code %v, but got %v", http.StatusOK, status)
+		}
+
+		expected_body := `{"test@test.com":{"email":"test@test.com","name":"test"}}`
+		response_body := app.recorder.Body.String()
+
+		if response_body != expected_body {
+			t.Errorf("Expected body %v, but got %v", expected_body, response_body)
+		}
+	})
 }
