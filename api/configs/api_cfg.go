bdfaf81712b16e37b8479eb146b723f9e2b16f70
solomonbaez <solomon.james.baez@gmail.com>
2023-09-17 19:32:07 -0700

    Added database and application setting generators to api_cfg

--- api/configs/api_cfg.go
+++ api/configs/api_cfg.go
@@ -1,7 +1,10 @@
 package configs
 
+import "fmt"
+
+// TODO export constants to a cfg.yml
 const (
-	APPLICATION_PORT = 8000
+	APP_PORT = 8000
 
 	DB_USER = "postgres"
 	DB_PASS = "password"
@@ -9,3 +12,40 @@ const (
 	DB_HOST = "localhost"
 	DB_PORT = 5432
 )
+
+type DBSettings struct {
+	user string
+	pass string
+	host string
+	port uint16
+	name string
+}
+
+func (db DBSettings) connection_string() string {
+	return fmt.Sprintf(
+		"postgres://%v:%v@%v:%v/%v",
+		db.user, db.pass, db.host, db.port, db.name,
+	)
+}
+
+type AppSettings struct {
+	database DBSettings
+	port     uint16
+}
+
+func ConfigureApp() AppSettings {
+	database := DBSettings{
+		DB_USER,
+		DB_PASS,
+		DB_HOST,
+		DB_PORT,
+		DB_NAME,
+	}
+
+	settings := AppSettings{
+		database,
+		APP_PORT,
+	}
+
+	return settings
+}
