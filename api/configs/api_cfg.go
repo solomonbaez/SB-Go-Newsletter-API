87a0563f42755a607e0fd124006114b17cbce5e2
solomonbaez <solomon.james.baez@gmail.com>
2023-09-17 17:35:21 -0700

    Added basic api config file

---
+++ api/configs/api_cfg.go
@@ -0,0 +1,11 @@
+package configs
+
+const (
+	APPLICATION_PORT = 8000
+
+	DB_USER = "postgres"
+	DB_PASS = "password"
+	DB_NAME = "newsletter"
+	DB_HOST = "localhost"
+	DB_PORT = 5432
+)
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -31,8 +31,8 @@ fi
 DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASS="${POSTGRES_PASS:=password}"
 DB_NAME="${POSTGRES_NAME:=newsletter}"
-DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
+DB_PORT="${POSTGRES_PORT:=5432}"
 SSLMODE="disable" 
 
 # DEV - remove test container
