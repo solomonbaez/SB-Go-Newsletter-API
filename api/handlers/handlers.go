7e3992db32709d52ff1ffee15e4792e52cb1718b
solomonbaez <solomon.james.baez@gmail.com>
2023-09-20 17:16:15 -0700

    Began implementing database functionality in /handlers

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -1,8 +1,9 @@
 package handlers
 
 import (
-	"fmt"
+	// "fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
@@ -25,55 +26,95 @@ const (
 	MaxNameLen  = 100
 )
 
-var subscribers = make(map[string]models.Subscriber)
-
 func (rh RouteHandler) Subscribe(c *gin.Context) {
-	var subscriber models.Subscriber
+	return
+	// var subscriber models.Subscriber
 
-	if e := c.ShouldBindJSON(&subscriber); e != nil {
-		response := "Could not subscribe"
+	// if e := c.ShouldBindJSON(&subscriber); e != nil {
+	// 	response := "Could not subscribe"
+	// 	log.Error().
+	// 		Err(e).
+	// 		Msg(response)
+
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": "Could not subscribe: " + e.Error()})
+	// 	return
+	// }
+
+	// if _, e := subscribers[subscriber.Email]; e {
+	// 	response := "Email already associated with a subscriber"
+	// 	log.Error().
+	// 		Msg(response)
+
+	// 	c.JSON(http.StatusFound, gin.H{"error": response})
+	// 	return
+
+	// } else if len(subscriber.Email) > MaxEmailLen {
+	// 	response := "Email exceeds the maximum limit of 100 characters"
+	// 	log.Error().
+	// 		Msg(response)
+
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": response})
+	// 	return
+
+	// } else if len(subscriber.Name) > MaxNameLen {
+	// 	response := "Name exceeds the maximum limit of 100 characters"
+	// 	log.Error().
+	// 		Msg(response)
+
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": response})
+	// 	return
+	// }
+
+	// subscribers[subscriber.Email] = subscriber
+	// log.Info().
+	// 	Str("email", subscriber.Email).
+	// 	Msg(fmt.Sprintf("%v subscribed!", subscriber.Email))
+
+	// c.JSON(http.StatusCreated, subscriber)
+}
+
+func (rh RouteHandler) GetSubscribers(c *gin.Context) {
+	var subscribers []models.Subscriber
+	rows, e := rh.DB.Query(c, "SELECT * FROM subscriptions")
+	if e != nil {
+		response := "Failed to fetch subscribers"
 		log.Error().
 			Err(e).
 			Msg(response)
 
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Could not subscribe: " + e.Error()})
+		c.JSON(http.StatusInternalServerError, response+" "+e.Error())
 		return
 	}
 
-	if _, e := subscribers[subscriber.Email]; e {
-		response := "Email already associated with a subscriber"
+	subscribers, e = pgx.CollectRows[models.Subscriber](rows, func(row pgx.CollectableRow) (models.Subscriber, error) {
+		var id int
+		var email string
+		var name string
+		var created time.Time
+
+		e := row.Scan(&id, &email, &name, &created)
+		s := models.Subscriber{
+			Email: email,
+			Name:  name,
+		}
+
+		return s, e
+	})
+	if e != nil {
+		response := "Failed to fetch subscribers"
 		log.Error().
 			Msg(response)
 
-		c.JSON(http.StatusFound, gin.H{"error": response})
-		return
-
-	} else if len(subscriber.Email) > MaxEmailLen {
-		response := "Email exceeds the maximum limit of 100 characters"
-		log.Error().
-			Msg(response)
-
-		c.JSON(http.StatusBadRequest, gin.H{"error": response})
-		return
-
-	} else if len(subscriber.Name) > MaxNameLen {
-		response := "Name exceeds the maximum limit of 100 characters"
-		log.Error().
-			Msg(response)
-
-		c.JSON(http.StatusBadRequest, gin.H{"error": response})
-		return
+		c.JSON(http.StatusBadRequest, response)
 	}
 
-	subscribers[subscriber.Email] = subscriber
-	log.Info().
-		Str("email", subscriber.Email).
-		Msg(fmt.Sprintf("%v subscribed!", subscriber.Email))
+	// TESTING
+	test_subscriber := models.Subscriber{
+		Email: "testing@testing.com",
+		Name:  "testing",
+	}
+	subscribers = append(subscribers, test_subscriber)
 
-	c.JSON(http.StatusCreated, subscriber)
-}
-
-func (rh RouteHandler) GetSubscribers(c *gin.Context) {
 	if len(subscribers) > 0 {
 		c.JSON(http.StatusOK, subscribers)
 	} else {
