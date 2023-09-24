a0d8b1c8a6854c97b0e1c2e6ff574a7d92bb3108
solomonbaez <solomon.james.baez@gmail.com>
2023-09-24 11:55:46 -0700

    Added password export to fix connection error

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -47,6 +47,7 @@ then
         -d postgres  
 fi
 
+export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
     >&2 echo "Postgres is still unavailable - sleeping"
     sleep 1
