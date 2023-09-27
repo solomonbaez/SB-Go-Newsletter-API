ec594b72227aee41027c41d6abea571987bea58b
solomonbaez <solomon.james.baez@gmail.com>
2023-09-26 17:22:56 -0700

    Refactoring EmailClient and helper functions in /email

--- api/email/email.go
+++ api/email/email.go
@@ -1,11 +1,16 @@
 package email
 
 import (
+	"github.com/rs/zerolog/log"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/models"
 )
 
 type EmailClient struct {
-	Sender models.SubscriberEmail
+	SMTPServer   string
+	SMTPPort     int
+	SMTPUsername string
+	SMTPassword  string
+	Sender       models.SubscriberEmail
 }
 
 type Email struct {
@@ -15,6 +20,26 @@ type Email struct {
 	Text      string
 }
 
+func NewEmailClient(
+	server string,
+	port int,
+	username string,
+	password string,
+	sender models.SubscriberEmail,
+) *EmailClient {
+	return &EmailClient{
+		SMTPServer:   server,
+		SMTPPort:     port,
+		SMTPUsername: username,
+		SMTPassword:  password,
+		Sender:       sender,
+	}
+}
+
 func (client EmailClient) SendEmail(email Email) Email {
+	log.Info().
+		Str("sender", client.Sender.String()).
+		Msg("Attempting to send an email")
+
 	return email
 }
