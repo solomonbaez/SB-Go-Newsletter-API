e2a00229914a937b736c55a69e9a1bc363a2a47a
solomonbaez <solomon.james.baez@gmail.com>
2023-10-03 14:03:18 -0700

    Moved validation logic to post_newsletters and integrated BasicAuth with credential validation

--- api/handlers/get_users.go
+++ api/handlers/get_users.go
@@ -1,21 +1,10 @@
 package handlers
 
 import (
-	"encoding/base64"
-	"errors"
-	"net/http"
-	"strings"
-	"unicode/utf8"
-
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
 	"github.com/rs/zerolog/log"
 )
-
-type Credentials struct {
-	Username string
-	Password string
-}
 
 func (rh *RouteHandler) GetUsers(c *gin.Context) (gin.Accounts, error) {
 	var users gin.Accounts
@@ -52,70 +41,4 @@ func (rh *RouteHandler) GetUsers(c *gin.Context) (gin.Accounts, error) {
 		Msg("Successfully fetched admin users")
 
 	return users, nil
-}
-
-func (rh *RouteHandler) ValidateCredentials(c *gin.Context, username string, password string) (*string, error) {
-	var id string
-
-	requestID := c.GetString("requestID")
-
-	query := "SELECT id FROM users WHERE username=$1 AND password=$2"
-	e := rh.DB.QueryRow(c, query, username, password).Scan(&id)
-	if e != nil {
-		log.Error().
-			Str("requestID", requestID).
-			Err(e).
-			Msg("Failed to validate user credentials")
-
-		return nil, e
-	}
-
-	log.Info().
-		Str("requestID", requestID).
-		Str("userID", id).
-		Msg("Successfully validated user credentials")
-
-	return &id, nil
-}
-
-func BasicAuth(c *gin.Context) (*gin.Accounts, error) {
-	var response string
-	var e error
-
-	requestID := c.GetString("requestID")
-	h := c.GetHeader("Authorization")
-
-	encodedSegment, valid := strings.CutPrefix(h, "Basic ")
-	if !valid {
-		e := errors.New("authorization method is not Basic")
-		response = "Incorrect authorization method"
-		HandleError(c, requestID, e, response, http.StatusBadRequest)
-		return nil, e
-	}
-
-	decodedSegment, e := base64.RawStdEncoding.DecodeString(encodedSegment)
-	if e != nil {
-		response = "Failed to decode header"
-		HandleError(c, requestID, e, response, http.StatusBadRequest)
-		return nil, e
-	}
-
-	valid = utf8.Valid(decodedSegment)
-	if !valid {
-		e = errors.New("invalid header encoding")
-		response = "Invalid header"
-		HandleError(c, requestID, e, response, http.StatusBadRequest)
-		return nil, e
-	}
-
-	// valid header should only contain two segments
-	utf8Segment := string(decodedSegment)
-	s := strings.Split(utf8Segment, ":")
-	username := s[0]
-	password := s[1]
-
-	credentials := make(gin.Accounts)
-	credentials[username] = password
-
-	return &credentials, nil
 }
--- api/handlers/post_newsletters.go
+++ api/handlers/post_newsletters.go
@@ -1,13 +1,23 @@
 package handlers
 
 import (
+	"encoding/base64"
+	"errors"
 	"fmt"
 	"net/http"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/clients"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/models"
 )
+
+type Credentials struct {
+	username string
+	password string
+}
 
 func (rh *RouteHandler) PostNewsletter(c *gin.Context, client *clients.SMTPClient) {
 	var newsletter models.Newsletter
@@ -16,6 +26,18 @@ func (rh *RouteHandler) PostNewsletter(c *gin.Context, client *clients.SMTPClien
 	var e error
 
 	requestID := c.GetString("requestID")
+	userCredentials, e := BasicAuth(c)
+	if e != nil {
+		response = "Unauthorized user"
+		HandleError(c, requestID, e, response, http.StatusBadRequest)
+		return
+	}
+
+	_, e = rh.ValidateCredentials(c, userCredentials)
+	if e != nil {
+		response := "Failed to validate credentials"
+		HandleError(c, requestID, e, response, http.StatusBadRequest)
+	}
 
 	if e = c.ShouldBindJSON(&body); e != nil {
 		response = "Could not send newsletter"
@@ -41,4 +63,59 @@ func (rh *RouteHandler) PostNewsletter(c *gin.Context, client *clients.SMTPClien
 	}
 
 	c.JSON(http.StatusOK, gin.H{"requestID": requestID, "message": "Emails successfully delivered"})
+}
+
+func (rh *RouteHandler) ValidateCredentials(c *gin.Context, credentials *Credentials) (*string, error) {
+	var id string
+
+	requestID := c.GetString("requestID")
+
+	query := "SELECT id FROM users WHERE username=$1 AND password=$2"
+	e := rh.DB.QueryRow(c, query, credentials.username, credentials.password).Scan(&id)
+	if e != nil {
+		return nil, e
+	}
+
+	log.Info().
+		Str("requestID", requestID).
+		Str("userID", id).
+		Msg("Successfully validated user credentials")
+
+	return &id, nil
+}
+
+func BasicAuth(c *gin.Context) (*Credentials, error) {
+	var e error
+
+	h := c.GetHeader("Authorization")
+
+	encodedSegment, valid := strings.CutPrefix(h, "Basic ")
+	if !valid {
+		e := errors.New("authorization method is not Basic")
+		return nil, e
+	}
+
+	decodedSegment, e := base64.RawStdEncoding.DecodeString(encodedSegment)
+	if e != nil {
+		return nil, e
+	}
+
+	valid = utf8.Valid(decodedSegment)
+	if !valid {
+		e = errors.New("invalid header encoding")
+		return nil, e
+	}
+
+	// valid header should only contain two segments
+	utf8Segment := string(decodedSegment)
+	s := strings.Split(utf8Segment, ":")
+	username := s[0]
+	password := s[1]
+
+	credentials := &Credentials{
+		username,
+		password,
+	}
+
+	return credentials, nil
 }
