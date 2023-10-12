ad27ab204f03ba114a632d326402217c25be974e
solomonbaez <solomon.james.baez@gmail.com>
2023-10-11 23:22:38 -0700

    Added GET and POST tests for /admin/login

--- tests/admin_test.go
+++ tests/admin_test.go
@@ -2,12 +2,17 @@ package api_test
 
 import (
 	"net/http"
+	"path/filepath"
 
 	"testing"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"github.com/pashagolub/pgxmock/v3"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/clients"
+	"github.com/solomonbaez/SB-Go-Newsletter-API/api/handlers"
+	"github.com/solomonbaez/SB-Go-Newsletter-API/api/routes"
 )
 
 // // Reference
@@ -17,7 +22,7 @@ import (
 // 	router   *gin.Engine
 // }
 
-func TestGetLogin(t *testing.T) {
+func Test_GetLogin(t *testing.T) {
 	// initialize
 	database, e := spawn_mock_database()
 	if e != nil {
@@ -43,12 +48,40 @@ func TestGetLogin(t *testing.T) {
 	if status := app.recorder.Code; status != http.StatusOK {
 		t.Errorf("Expected status code %v, but got %v", http.StatusOK, status)
 	}
+}
 
-	// expected_body := `{"requestID":"","subscribers":"No subscribers"}`
-	// response_body := app.recorder.Body.String()
-	// if response_body != expected_body {
-	// 	t.Errorf("Expected body %v, but got %v", expected_body, response_body)
-	// }
+func Test_PostLogin_Passes(t *testing.T) {
+	// initialize
+	database, e := spawn_mock_database()
+	if e != nil {
+		t.Fatal(e)
+	}
+	client, e := spawn_mock_smtp_client()
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	router := admin_spawn_mock_router(database, client)
+
+	request, e := http.NewRequest("POST", "/admin/login", nil)
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	database.ExpectQuery(`SELECT id, password_hash FROM users WHERE user = $1`).
+		WithArgs(pgxmock.AnyArg()).
+		WillReturnRows(
+			pgxmock.NewRows([]string{"id", "password_hash"}),
+		)
+
+	app := spawn_app(router, request)
+	defer database.ExpectationsWereMet()
+	defer database.Close(app.context)
+
+	// tests
+	if status := app.recorder.Code; status != http.StatusSeeOther {
+		t.Errorf("Expected status code %v, but got %v", http.StatusOK, status)
+	}
 }
 
 // // Reference
@@ -72,11 +105,26 @@ func TestGetLogin(t *testing.T) {
 // }
 
 func admin_spawn_mock_router(db pgxmock.PgxConnIface, client *clients.SMTPClient) *gin.Engine {
-	// rh := handlers.NewRouteHandler(db)
+	rh := handlers.NewRouteHandler(db)
+
+	key1, _ := handlers.GenerateCSPRNG(32)
+	key2, _ := handlers.GenerateCSPRNG(32)
+	storeKeys := [][]byte{
+		[]byte(key1),
+		[]byte(key2),
+	}
+	store := cookie.NewStore(storeKeys...)
 
 	router := gin.Default()
+	router.LoadHTMLGlob(filepath.Join("../api/templates", "*"))
+
+	router.Use(sessions.Sessions("test", store))
+
 	admin := router.Group("/admin")
-	admin.GET("/login")
+	admin.GET("/login", routes.GetLogin)
+	admin.POST("/login", func(c *gin.Context) {
+		routes.PostLogin(c, rh)
+	})
 
 	return router
 }
