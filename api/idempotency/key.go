fc43ab03a10a05e6775e1240afe3dcf4db530e6a
solomonbaez <solomon.james.baez@gmail.com>
2023-10-13 17:58:50 -0700

    Added basic Idempotency Key generator

---
+++ api/idempotency/key.go
@@ -0,0 +1,17 @@
+package idempotency
+
+import "github.com/solomonbaez/SB-Go-Newsletter-API/api/handlers"
+
+const keyLen = 25
+
+type IdempotencyKey string
+
+func GenerateIdempotencyKey() (*IdempotencyKey, error) {
+	key, e := handlers.GenerateCSPRNG(keyLen)
+	if e != nil {
+		return nil, e
+	}
+
+	idempotencyKey := IdempotencyKey(key)
+	return &idempotencyKey, nil
+}
