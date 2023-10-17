67faec361e144f5d1fee4498c9568f0751d5eaf9
solomonbaez <solomon.james.baez@gmail.com>
2023-10-16 22:19:15 -0700

    Working through implementing concurrent delivery worker logic

--- api/idempotency/concurrency.go
+++ api/idempotency/concurrency.go
@@ -7,11 +7,8 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
-	"github.com/rs/zerolog/log"
 
-	"github.com/solomonbaez/SB-Go-Newsletter-API/api/clients"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/handlers"
-	"github.com/solomonbaez/SB-Go-Newsletter-API/api/models"
 )
 
 type NextAction struct {
@@ -54,102 +51,4 @@ func TryProcessing(c *gin.Context, dh *handlers.DatabaseHandler) (*NextAction, e
 	}
 
 	return &NextAction{SavedResponse: savedResponse}, nil
-}
-
-func EnqueDeliveryTasks(c *gin.Context, tx pgx.Tx, newsletterIssueId string) error {
-	query := `INSERT INTO issue_delivery_queue (
-				newsletter_issue_id,
-				subscriber_email
-			)
-			SELECT $1, email
-			FROM subscriptions
-			WHERE status = 'confirmed'`
-	_, e := tx.Exec(c, query, newsletterIssueId)
-	if e != nil {
-		return e
-	}
-
-	tx.Commit(c)
-	return nil
-}
-
-// TODO implement n_retries + execute_after columns to issue_delivery_queue to attempt retries
-func TryExecuteTask(c *gin.Context, dh *handlers.DatabaseHandler, client *clients.SMTPClient) (e error) {
-	issueID, subscriberEmail, tx, e := DequeTask(c, dh)
-	if e != nil {
-		return e
-	}
-
-	content, e := GetIssue(c, tx, *issueID)
-	if e != nil {
-		return e
-	}
-
-	// re-parse email to ensure data integrity
-	var newsletter models.Newsletter
-	newsletter.Recipient, e = models.ParseEmail(*subscriberEmail)
-	if e != nil {
-		return e
-	}
-	newsletter.Content = content
-	if e = models.ParseNewsletter(&newsletter); e != nil {
-		return e
-	}
-	if e = client.SendEmail(&newsletter); e != nil {
-		return e
-	}
-
-	if e = DeleteTask(c, tx, *issueID, *subscriberEmail); e != nil {
-		return e
-	}
-
-	log.Info().
-		Str("subscriber", *subscriberEmail).
-		Msg("Email sent")
-
-	return nil
-}
-
-func DequeTask(c *gin.Context, dh *handlers.DatabaseHandler) (issueID, subscriberEmail *string, tx pgx.Tx, e error) {
-	tx, e = dh.DB.Begin(c)
-	if e != nil {
-		return nil, nil, nil, e
-	}
-
-	query := `SELECT newsletter_issue_id, subscriber_email
-			FROM issue_delivery_queue
-			FOR UPDATE
-			SKIP LOCKED
-			LIMIT 1`
-	e = tx.QueryRow(c, query).Scan(&issueID, &subscriberEmail)
-	if e != nil {
-		return nil, nil, nil, e
-	}
-
-	return issueID, subscriberEmail, tx, nil
-}
-
-func GetIssue(c *gin.Context, tx pgx.Tx, issueID string) (content *models.Body, e error) {
-	query := `SELECT title, text_content, html_content
-			FROM newsletter_issues
-			WHERE newsletter_issue_id = $1`
-	if e = tx.QueryRow(c, query, issueID).Scan(&content.Title, &content.Text, &content.Html); e != nil {
-		return nil, e
-	}
-
-	return content, nil
-}
-
-func DeleteTask(c *gin.Context, tx pgx.Tx, issueID, subscriberEmail string) (e error) {
-	query := `DELETE FROM issue_delivery_queue
-			WHERE 
-			newsletter_issue_id = $1 AND
-			subscriber_email = $2`
-	_, e = tx.Exec(c, query, issueID, subscriberEmail)
-	if e != nil {
-		return e
-	}
-
-	tx.Commit(c)
-	return nil
 }
--- api/idempotency/worker.go
+++ api/idempotency/worker.go
@@ -4,19 +4,166 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"github.com/rs/zerolog/log"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/clients"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/handlers"
+	"github.com/solomonbaez/SB-Go-Newsletter-API/api/models"
 )
 
-func DeliveryWorker(c *gin.Context, dh *handlers.DatabaseHandler, client *clients.SMTPClient) {
-	var e error
-	go func() {
-		if e = TryExecuteTask(c, dh, client); e != nil {
-			if e.Error() == "No rows in result set" {
+type ExecutionOutcome int
+
+const (
+	ExecutionOutcomeEmptyQueue ExecutionOutcome = iota
+	ExecutionOutcomeError
+	ExecutionOutcomeTaskCompleted
+)
+
+func WorkerLoop(c *gin.Context, dh *handlers.DatabaseHandler, client *clients.SMTPClient) {
+	for {
+		result := TryExecuteTask(c, dh, client)
+		select {
+		case outcome := <-result:
+			switch outcome {
+			case ExecutionOutcomeEmptyQueue:
 				time.Sleep(10 * time.Second)
-			} else {
-				time.Sleep(time.Second)
+
+			case ExecutionOutcomeError:
+				time.Sleep(1 * time.Second)
+
+			case ExecutionOutcomeTaskCompleted:
 			}
 		}
+	}
+}
+
+type Task struct {
+	NewsletterIssueID string
+	SubscriberEmail   models.SubscriberEmail
+}
+
+// TODO implement n_retries + execute_after columns to issue_delivery_queue to attempt retries
+func TryExecuteTask(c *gin.Context, dh *handlers.DatabaseHandler, client *clients.SMTPClient) chan ExecutionOutcome {
+	resultChan := make(chan ExecutionOutcome)
+	go func() {
+		task, tx, e := DequeTask(c, dh)
+		if e != nil {
+			log.Error().
+				Err(e).
+				Msg(e.Error())
+			resultChan <- ExecutionOutcomeEmptyQueue
+			return
+		}
+
+		content, e := GetIssue(c, tx, task.NewsletterIssueID)
+		if e != nil {
+			log.Error().
+				Err(e).
+				Msg(e.Error())
+			resultChan <- ExecutionOutcomeError
+			return
+		}
+
+		// re-parse email to ensure data integrity
+		var newsletter models.Newsletter
+		newsletter.Recipient, e = models.ParseEmail(task.SubscriberEmail.String())
+		if e != nil {
+			log.Error().
+				Err(e).
+				Msg(e.Error())
+			resultChan <- ExecutionOutcomeError
+			return
+		}
+		newsletter.Content = content
+		if e = models.ParseNewsletter(&newsletter); e != nil {
+			log.Error().
+				Err(e).
+				Msg(e.Error())
+			resultChan <- ExecutionOutcomeError
+			return
+		}
+		if e = client.SendEmail(&newsletter); e != nil {
+			log.Error().
+				Err(e).
+				Msg(e.Error())
+			resultChan <- ExecutionOutcomeError
+			return
+		}
+
+		if e = DeleteTask(c, tx, task); e != nil {
+			log.Error().
+				Err(e).
+				Msg(e.Error())
+			resultChan <- ExecutionOutcomeError
+			return
+		}
+
+		log.Info().
+			Str("subscriber", task.SubscriberEmail.String()).
+			Msg("Email sent")
+
+		resultChan <- ExecutionOutcomeTaskCompleted
 	}()
+	return resultChan
+}
+
+func DequeTask(c *gin.Context, dh *handlers.DatabaseHandler) (task *Task, tx pgx.Tx, e error) {
+	tx, e = dh.DB.Begin(c)
+	if e != nil {
+		return nil, nil, e
+	}
+
+	query := `SELECT newsletter_issue_id, subscriber_email
+			FROM issue_delivery_queue
+			FOR UPDATE
+			SKIP LOCKED
+			LIMIT 1`
+	e = tx.QueryRow(c, query).Scan(&task.NewsletterIssueID, &task.SubscriberEmail)
+	if e != nil {
+		return nil, nil, e
+	}
+
+	return task, tx, nil
+}
+
+func GetIssue(c *gin.Context, tx pgx.Tx, issueID string) (content *models.Body, e error) {
+	query := `SELECT title, text_content, html_content
+			FROM newsletter_issues
+			WHERE newsletter_issue_id = $1`
+	if e = tx.QueryRow(c, query, issueID).Scan(&content.Title, &content.Text, &content.Html); e != nil {
+		return nil, e
+	}
+
+	return content, nil
+}
+
+func DeleteTask(c *gin.Context, tx pgx.Tx, task *Task) (e error) {
+	query := `DELETE FROM issue_delivery_queue
+			WHERE 
+			newsletter_issue_id = $1 AND
+			subscriber_email = $2`
+	_, e = tx.Exec(c, query, task.NewsletterIssueID, task.SubscriberEmail.String())
+	if e != nil {
+		return e
+	}
+
+	tx.Commit(c)
+	return nil
+}
+
+func EnqueDeliveryTasks(c *gin.Context, tx pgx.Tx, newsletterIssueId string) error {
+	query := `INSERT INTO issue_delivery_queue (
+				newsletter_issue_id,
+				subscriber_email
+			)
+			SELECT $1, email
+			FROM subscriptions
+			WHERE status = 'confirmed'`
+	_, e := tx.Exec(c, query, newsletterIssueId)
+	if e != nil {
+		return e
+	}
+
+	tx.Commit(c)
+	return nil
 }
