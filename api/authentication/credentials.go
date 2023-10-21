4c8913076222a2ce8587d543b5e21bb395ee2f45
solomonbaez <solomon.james.baez@gmail.com>
2023-10-21 13:19:27 -0700

    Began updating error messages and handling

--- api/authentication/credentials.go
+++ api/authentication/credentials.go
@@ -7,20 +7,27 @@ import (
 
 	"strings"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/rs/zerolog/log"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/handlers"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/models"
 )
 
 // TODO Sanitize credentials again?
-func ValidateCredentials(c context.Context, dh *handlers.DatabaseHandler, credentials *models.Credentials) (id *string, e error) {
+func ValidateCredentials(c context.Context, dh *handlers.DatabaseHandler, credentials *models.Credentials) (id *string, err error) {
 	var userID string
 	var passwordHash string
 
 	var user_e error
 	query := "SELECT id, password_hash FROM users WHERE username=$1"
-	e = dh.DB.QueryRow(c, query, credentials.Username).Scan(&userID, &passwordHash)
+	e := dh.DB.QueryRow(c, query, credentials.Username).Scan(&userID, &passwordHash)
 	if e != nil {
+		if errors.Is(e, pgx.ErrNoRows) {
+			e = errors.New("user not found")
+		} else {
+			e = fmt.Errorf("database error: %w", e)
+		}
+
 		log.Error().
 			Err(e).
 			Msg("Invalid username")
@@ -33,6 +40,8 @@ func ValidateCredentials(c context.Context, dh *handlers.DatabaseHandler, creden
 	if e = models.ValidatePHC(credentials.Password, passwordHash); e != nil {
 		if user_e != nil {
 			e = user_e
+		} else {
+			e = fmt.Errorf("invalid credentials: %w", e)
 		}
 		return nil, e
 	}
@@ -41,12 +50,12 @@ func ValidateCredentials(c context.Context, dh *handlers.DatabaseHandler, creden
 	return id, nil
 }
 
-func ParseField(field string) (parsed *string, e error) {
+func ParseField(field string) (parsed *string, err error) {
 	// injection check
 	for _, r := range field {
 		c := string(r)
 		if strings.Contains(models.InvalidRunes, c) {
-			return nil, fmt.Errorf("invalid character in field: %v", c)
+			return nil, fmt.Errorf("invalid character in field: %s", c)
 		}
 	}
 
