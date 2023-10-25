c87553561e9793e3d1371d56f8fb003c8588c683
solomonbaez <solomon.james.baez@gmail.com>
2023-10-24 21:40:46 -0700

    Sketching out naive key pruning function with worker loop integration

--- api/workers/worker.go
+++ api/workers/worker.go
@@ -7,6 +7,7 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/clients"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/handlers"
+	"github.com/solomonbaez/SB-Go-Newsletter-API/api/idempotency"
 )
 
 type ExecutionOutcome int
@@ -38,6 +39,69 @@ func WorkerLoop(c context.Context, dh *handlers.DatabaseHandler, client *clients
 		case ExecutionOutcomeTaskCompleted:
 			log.Info().
 				Msg("Task complete")
+		}
+	}
+}
+
+func WorkerLoopWithPruning(c context.Context, dh *handlers.DatabaseHandler, client *clients.SMTPClient) {
+	resultChan := make(chan ExecutionOutcome)
+	keyCleanupTimer := time.NewTimer(24 * time.Hour)
+
+	for {
+		select {
+		case <-keyCleanupTimer.C:
+			tryKeyCleanup(c, dh)
+			keyCleanupTimer.Reset(24 * time.Hour)
+
+		case resultChan <- TryExecuteTask(c, dh, client):
+			outcome := <-resultChan
+			switch outcome {
+			case ExecutionOutcomeEmptyQueue:
+				log.Info().
+					Msg("Empty queue")
+				time.Sleep(10 * time.Second)
+			case ExecutionOutcomeError:
+				log.Error().
+					Msg("Failed to complete task")
+				time.Sleep(1 * time.Second)
+			case ExecutionOutcomeTaskCompleted:
+				log.Info().
+					Msg("Task complete")
+			}
+		}
+	}
+}
+
+func tryKeyCleanup(c context.Context, dh *handlers.DatabaseHandler) {
+	ticker := time.NewTicker(24 * time.Hour)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			expiration := time.Now().Add(-24 * time.Hour)
+
+			_, cancel := context.WithTimeout(c, 10*time.Second)
+			defer cancel()
+
+			tx, e := dh.DB.Begin(c)
+			if e != nil {
+				log.Error().
+					Err(e).
+					Msg("")
+
+				cancel()
+			}
+
+			if e := idempotency.PruneIdempotencyKeys(c, tx, expiration); e != nil {
+				log.Error().
+					Err(e).
+					Msg("")
+
+				cancel()
+			}
+
+			tx.Commit(c)
 		}
 	}
 }
