716bd257f9d82183c588f414e62af2d8a5d0159d
solomonbaez <solomon.james.baez@gmail.com>
2023-10-24 21:10:26 -0700

    Sketched out idempotency key deletion for gin based request retries with expired keys

--- api/idempotency/expiry.go
+++ api/idempotency/expiry.go
@@ -1,9 +1,45 @@
 package idempotency
 
 import (
-	"github.com/solomonbaez/SB-Go-Newsletter-API/api/handlers"
+	"fmt"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"github.com/rs/zerolog/log"
 )
 
-func checkExpiry(c context.Context, dh *handlers.DatabaseHandler) (err error) {
+// Implemented as a fallback on request retries
+func DeleteIdempotencyKey(c *gin.Context, tx pgx.Tx, key IdempotencyKey) (err error) {
+	session := sessions.Default(c)
+	id := fmt.Sprintf("%s", session.Get("id"))
 
+	query := "DELETE FROM idempotency WHERE id = $1 AND idempotency_key = $2"
+	_, e := tx.Exec(c, query, id, key)
+	if e != nil {
+		err = fmt.Errorf("failed to delete idempotency key: %w", e)
+		log.Error().
+			Err(err).
+			Msg("")
+
+		return
+	}
+
+	query = "DELETE FROM idempotency_headers WHERE idempotency_key = $1"
+	_, e = tx.Exec(c, query, key)
+	if e != nil {
+		err = fmt.Errorf("failed to delete saved idempotency headers: %w", e)
+		log.Error().
+			Err(err).
+			Msg("")
+
+		return
+	}
+
+	log.Info().
+		Str("id", id).
+		Str("key", key.String()).
+		Msg("Key expired")
+
+	return
 }
