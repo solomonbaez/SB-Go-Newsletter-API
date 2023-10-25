481dcabebe210aee9b459f3b51572db87de96e85
solomonbaez <solomon.james.baez@gmail.com>
2023-10-24 21:15:52 -0700

    Sketched out idempotency key pruning for worker operations

--- api/idempotency/expiry.go
+++ api/idempotency/expiry.go
@@ -1,7 +1,9 @@
 package idempotency
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -17,7 +19,7 @@ func DeleteIdempotencyKey(c *gin.Context, tx pgx.Tx, key IdempotencyKey) (err er
 	query := "DELETE FROM idempotency WHERE id = $1 AND idempotency_key = $2"
 	_, e := tx.Exec(c, query, id, key)
 	if e != nil {
-		err = fmt.Errorf("failed to delete idempotency key: %w", e)
+		err = fmt.Errorf("failed to delete expired idempotency key: %w", e)
 		log.Error().
 			Err(err).
 			Msg("")
@@ -28,7 +30,7 @@ func DeleteIdempotencyKey(c *gin.Context, tx pgx.Tx, key IdempotencyKey) (err er
 	query = "DELETE FROM idempotency_headers WHERE idempotency_key = $1"
 	_, e = tx.Exec(c, query, key)
 	if e != nil {
-		err = fmt.Errorf("failed to delete saved idempotency headers: %w", e)
+		err = fmt.Errorf("failed to delete expired idempotency headers: %w", e)
 		log.Error().
 			Err(err).
 			Msg("")
@@ -40,6 +42,36 @@ func DeleteIdempotencyKey(c *gin.Context, tx pgx.Tx, key IdempotencyKey) (err er
 		Str("id", id).
 		Str("key", key.String()).
 		Msg("Key expired")
+
+	return
+}
+
+// Implemented as a general purpose database sweep
+func PruneIdempotencyKeys(c context.Context, tx pgx.Tx, expiration time.Time) (err error) {
+	query := "DELETE FROM idempotency WHERE created <= $1"
+	_, e := tx.Exec(c, query, expiration)
+	if e != nil {
+		err = fmt.Errorf("failed to prune expired idempotency keys: %w", e)
+		log.Error().
+			Err(err).
+			Msg("")
+
+		return
+	}
+
+	query = "DELETE FROM idempotency_headers WHERE created <= $1"
+	_, e = tx.Exec(c, query, expiration)
+	if e != nil {
+		err = fmt.Errorf("failed to prune expired idempotency headers: %w", e)
+		log.Error().
+			Err(err).
+			Msg("")
+
+		return
+	}
+
+	log.Info().
+		Msg("Successfully pruned idempotency keys")
 
 	return
 }
