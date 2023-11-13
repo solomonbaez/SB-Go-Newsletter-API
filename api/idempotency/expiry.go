8ccf4ef02c14e03085ffe66d2f248f4ce8625e08
solomonbaez <solomon.james.baez@gmail.com>
2023-11-12 20:01:15 -0800

    Removed unused pruning function

--- api/idempotency/expiry.go
+++ api/idempotency/expiry.go
@@ -5,48 +5,11 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
 	"github.com/rs/zerolog/log"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/handlers"
 	"github.com/solomonbaez/SB-Go-Newsletter-API/api/models"
 )
-
-// Implemented as a fallback on request retries
-func DeleteIdempotencyKey(c *gin.Context, tx pgx.Tx, key IdempotencyKey) (err error) {
-	session := sessions.Default(c)
-	id := fmt.Sprintf("%s", session.Get("id"))
-
-	query := "DELETE FROM idempotency WHERE id = $1 AND idempotency_key = $2"
-	_, e := tx.Exec(c, query, id, key)
-	if e != nil {
-		err = fmt.Errorf("failed to delete expired idempotency key: %w", e)
-		log.Error().
-			Err(err).
-			Msg("")
-
-		return
-	}
-
-	query = "DELETE FROM idempotency_headers WHERE idempotency_key = $1"
-	_, e = tx.Exec(c, query, key)
-	if e != nil {
-		err = fmt.Errorf("failed to delete expired idempotency headers: %w", e)
-		log.Error().
-			Err(err).
-			Msg("")
-
-		return
-	}
-
-	log.Info().
-		Str("id", id).
-		Str("key", key.String()).
-		Msg("Key expired")
-
-	return
-}
 
 // Implemented as a general purpose database sweep
 func PruneIdempotencyKeys(c context.Context, dh *handlers.DatabaseHandler, expiration time.Time) (err error) {
