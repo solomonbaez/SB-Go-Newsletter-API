85005ecfda2f99fa3617983ea1ab0ab3bb2f6981
solomonbaez <solomon.james.baez@gmail.com>
2023-11-30 16:19:47 -0800

    Refining newsletter retrieval functionality

--- api/blog/getNewsletterIssue.go
+++ api/blog/getNewsletterIssue.go
@@ -21,7 +21,7 @@ func GetNewlsetterIssues(c *gin.Context, dh *handlers.DatabaseHandler) {
 		Msg("Fetching newsletter issues...")
 
 	var response string
-	rows, e := dh.DB.Query(c, "SELECT * FROM newsletter_issues")
+	rows, e := dh.DB.Query(c, "SELECT (title, text_content, html_content) FROM newsletter_issues")
 	if e != nil {
 		response = "Failed to fetch newsletter issues"
 		handlers.HandleError(c, requestID, e, response, http.StatusInternalServerError)
@@ -45,6 +45,29 @@ func GetNewlsetterIssues(c *gin.Context, dh *handlers.DatabaseHandler) {
 
 	c.JSON(http.StatusOK, gin.H{"requestID": requestID, "newsletterIssues": newsletterIssues})
 }
+
+
+func GetNewlsetterIssueByTitle(c *gin.Context, dh *handlers.DatabaseHandler) {
+  var newsletter *models.Newsletter
+  
+  requestID := c.GetString("requestID")
+  title := c.Param("title")
+
+	log.Info().
+		Str("requestID", requestID).
+		Msg("Fetching newsletter issues...")
+
+	var response string
+	e := dh.DB.QueryRow(c, "SELECT (title, text_content, html_content) FROM newsletter_issues WHERE title=$1", title).
+    Scan(&newsletter.Content.Title, &newsletter.Content.Text, &newsletter.Content.Html)
+	if e != nil {
+		response = "Failed to fetch newsletter"
+		handlers.HandleError(c, requestID, e, response, http.StatusInternalServerError)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"requestID": requestID, "newsletter": newsletter})
+}
    
 func buildNewsletter(row pgx.CollectableRow) (newsletter *models.Newsletter, err error) {
   var title string
@@ -53,10 +76,10 @@ func buildNewsletter(row pgx.CollectableRow) (newsletter *models.Newsletter, err
 
 	if e := row.Scan(&title, &text, &html); e != nil {
 		err = fmt.Errorf("database error: %w", e)
-		return
-	}
+    return
+  }
 
-  newsletter.Content = *models.Body{
+  newsletter.Content = &models.Body{
     Title: title,
     Text: text,
     Html: html,
